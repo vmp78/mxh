28ab57b06ebfc866b45b3aecf0d05977fd852cf7
Nguyen Dinh Phuc <hanoi2003.a@gmail.com>
2024-04-08 23:15:00 +0700

    Done user's post page

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,7 +1,25 @@
 import UserHeader from '../components/UserHeader';
+import UserPost from '../components/UserPost';
 
 const UserPage = () => {
-    return <UserHeader />;
+    return (
+        <>
+            <UserHeader />
+            <UserPost
+                likesCount={1200}
+                repliesCount={1000}
+                postImg={'/public/post1.jpg'}
+                postTitle={'4K laptop background'}
+            />
+            <UserPost likesCount={120} repliesCount={10} postImg={'/public/post2.JPG'} postTitle={'Critianoooooo'} />
+            <UserPost
+                likesCount={110}
+                repliesCount={40}
+                // postImg={'/public/post3.JPG'}
+                postTitle={'Spiderman comes home!'}
+            />
+        </>
+    );
 };
 
 export default UserPage;

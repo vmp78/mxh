ab95a2b09a3cd493f143869b5a7716391ac3e147
Nguyen Dinh Phuc <hanoi2003.a@gmail.com>
2024-04-10 22:53:53 +0700

    Connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './db/connectDB.js';
 
 dotenv.config();
+connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;

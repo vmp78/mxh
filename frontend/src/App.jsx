973d68948496d535408aadbe2e89d9cebb5ebdbb
Nguyen Dinh Phuc <hanoi2003.a@gmail.com>
2024-05-30 09:24:01 +0700

    Update header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,48 @@
-import { Box, Container } from "@chakra-ui/react";
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
-import UserPage from "./pages/UserPage";
-import PostPage from "./pages/PostPage";
-import Header from "./components/Header";
-import HomePage from "./pages/HomePage";
-import AuthPage from "./pages/AuthPage";
-import { useRecoilValue } from "recoil";
-import userAtom from "./atoms/userAtom";
-import CreatePost from "./components/CreatePost";
-import ChatPage from "./pages/ChatPage";
-import { SettingsPage } from "./pages/SettingsPage";
-import EditProfilePage from "./pages/EditProfilePage";
+import { Box, Container } from '@chakra-ui/react';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import UserPage from './pages/UserPage';
+import PostPage from './pages/PostPage';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import AuthPage from './pages/AuthPage';
+import { useRecoilValue } from 'recoil';
+import userAtom from './atoms/userAtom';
+import CreatePost from './components/CreatePost';
+import ChatPage from './pages/ChatPage';
+import { SettingsPage } from './pages/SettingsPage';
+import EditProfilePage from './pages/EditProfilePage';
 function App() {
-	const user = useRecoilValue(userAtom);
-	const { pathname } = useLocation();
-	return (
-		<Box position={"relative"} w='full'>
-			<Container maxW={pathname === "/" ? { base: "620px", md: "900px" } : "620px"}>
-				<Header />
-				<Routes>
-					<Route path='/' element={user ? <HomePage /> : <Navigate to='/auth' />} />
-					<Route path='/auth' element={!user ? <AuthPage /> : <Navigate to='/' />} />
-					<Route path='/update' element={user ? <EditProfilePage/> : <Navigate to='/auth' />} />
+    const user = useRecoilValue(userAtom);
+    const { pathname } = useLocation();
+    return (
+        <Box position={'relative'} w="full">
+            <Header />
+            <Container maxW={pathname === '/' ? { base: '620px', md: '900px' } : '620px'}>
+                <Routes>
+                    <Route path="/" element={user ? <HomePage /> : <Navigate to="/auth" />} />
+                    <Route path="/auth" element={!user ? <AuthPage /> : <Navigate to="/" />} />
+                    <Route path="/update" element={user ? <EditProfilePage /> : <Navigate to="/auth" />} />
 
-					<Route
-						path='/:username'
-						element={
-							user ? (
-								<>
-									<UserPage />
-									<CreatePost />
-								</>
-							) : (
-								<UserPage />
-							)
-						}
-					/>
-					<Route path='/:username/post/:pid' element={<PostPage />} />
-					<Route path='/chat' element={user ? <ChatPage /> : <Navigate to={"/auth"} />} />
-					<Route path='/settings' element={user ? <SettingsPage /> : <Navigate to={"/auth"} />} />
-				</Routes>
-			</Container>
-		</Box>
-	);
+                    <Route
+                        path="/:username"
+                        element={
+                            user ? (
+                                <>
+                                    <UserPage />
+                                    <CreatePost />
+                                </>
+                            ) : (
+                                <UserPage />
+                            )
+                        }
+                    />
+                    <Route path="/:username/post/:pid" element={<PostPage />} />
+                    <Route path="/chat" element={user ? <ChatPage /> : <Navigate to={'/auth'} />} />
+                    <Route path="/settings" element={user ? <SettingsPage /> : <Navigate to={'/auth'} />} />
+                </Routes>
+            </Container>
+        </Box>
+    );
 }
 
 export default App;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,66 +1,81 @@
-import { Button, Flex, Image, Link, useColorMode } from "@chakra-ui/react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import userAtom from "../atoms/userAtom";
-import { AiFillHome } from "react-icons/ai";
-import { RxAvatar } from "react-icons/rx";
-import { Link as RouterLink } from "react-router-dom";
-import { FiLogOut } from "react-icons/fi";
-import useLogout from "../hooks/useLogout";
-import authScreenAtom from "../atoms/authAtom";
-import { BsFillChatQuoteFill } from "react-icons/bs";
-import { MdOutlineSettings } from "react-icons/md";
+import { HamburgerIcon } from '@chakra-ui/icons';
+import { CSSReset, Flex, IconButton, Input, Link, Menu, MenuButton, MenuItem, MenuList, Text } from '@chakra-ui/react';
+import { AiFillHome } from 'react-icons/ai';
+import { LuLogOut, LuMessageCircle, LuSettings, LuUser } from 'react-icons/lu';
+import { Link as RouterLink } from 'react-router-dom';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import authScreenAtom from '../atoms/authAtom';
+import userAtom from '../atoms/userAtom';
+import useLogout from '../hooks/useLogout';
+
+const MenuItemHoverStyles = {
+    _hover: {
+        backgroundColor: 'gray.800',
+    },
+};
 
 const Header = () => {
-	const { colorMode, toggleColorMode } = useColorMode();
-	const user = useRecoilValue(userAtom);
-	const logout = useLogout();
-	const setAuthScreen = useSetRecoilState(authScreenAtom);
+    const user = useRecoilValue(userAtom);
+    const logout = useLogout();
+    const setAuthScreen = useSetRecoilState(authScreenAtom);
 
-	return (
-		<Flex justifyContent={"space-between"} mt={8} mb='12'>
-			{user && (
-				<Link as={RouterLink} to='/'>
-					<AiFillHome size={26} />
-				</Link>
-			)}
-			{!user && (
-				<Link as={RouterLink} to={"/auth"} onClick={() => setAuthScreen("login")}>
-					Login
-				</Link>
-			)}
+    return (
+        <>
+            <CSSReset />
+            <Flex
+                justifyContent="center"
+                ml={'auto'}
+                mr={'auto'}
+                maxW={{ base: '620px', md: '900px' }}
+                mt={6}
+                mb={10}
+                gap={20}
+            >
+                {user && (
+                    <Link as={RouterLink} to="/">
+                        <AiFillHome size={26} />
+                    </Link>
+                )}
+                {!user && (
+                    <Link as={RouterLink} to={'/auth'} onClick={() => setAuthScreen('login')}>
+                        Login
+                    </Link>
+                )}
 
-			<Image
-				cursor={"pointer"}
-				alt='logo'
-				w={6}
-				src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
-				onClick={toggleColorMode}
-			/>
+                {user && <Input placeholder="Search for an account" w="400px" />}
 
-			{user && (
-				<Flex alignItems={"center"} gap={10}>
-					<Link as={RouterLink} to={`/${user.username}`}>
-						<RxAvatar size={26} />
-					</Link>
-					<Link as={RouterLink} to={`/chat`}>
-						<BsFillChatQuoteFill size={26} />
-					</Link>
-					<Link as={RouterLink} to={`/settings`}>
-						<MdOutlineSettings size={26} />
-					</Link>
-					<Button size={"xs"} onClick={logout}>
-						<FiLogOut size={20} />
-					</Button>
-				</Flex>
-			)}
+                {user && (
+                    <Menu>
+                        <MenuButton as={IconButton} aria-label="Options" icon={<HamburgerIcon />} variant="outline" />
+                        <MenuList bg="black">
+                            <MenuItem as={RouterLink} to={`/${user.username}`} bg="black" _hover={MenuItemHoverStyles}>
+                                <LuUser size={26} />
+                                <Text ml={2}>Profile</Text>
+                            </MenuItem>
+                            <MenuItem as={RouterLink} to={`/chat`} bg="black" _hover={MenuItemHoverStyles}>
+                                <LuMessageCircle size={26} />
+                                <Text ml={2}>Chat</Text>
+                            </MenuItem>
+                            <MenuItem as={RouterLink} to={`/settings`} bg="black" _hover={MenuItemHoverStyles}>
+                                <LuSettings size={26} />
+                                <Text ml={2}>Settings</Text>
+                            </MenuItem>
+                            <MenuItem onClick={logout} bg="black" _hover={MenuItemHoverStyles}>
+                                <LuLogOut size={26} />
+                                <Text ml={2}>Log out</Text>
+                            </MenuItem>
+                        </MenuList>
+                    </Menu>
+                )}
 
-			{!user && (
-				<Link as={RouterLink} to={"/auth"} onClick={() => setAuthScreen("signup")}>
-					Sign up
-				</Link>
-			)}
-		</Flex>
-	);
+                {!user && (
+                    <Link as={RouterLink} to={'/auth'} onClick={() => setAuthScreen('signup')}>
+                        Sign up
+                    </Link>
+                )}
+            </Flex>
+        </>
+    );
 };
 
 export default Header;

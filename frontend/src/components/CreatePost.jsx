9e25fd1d7824277941f5f2f51dcedb16127b968b
JVCP223 <153863586+JVCP223@users.noreply.github.com>
2024-06-11 17:04:47 +0700

    Fix reloading new post

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -21,9 +21,10 @@ import {
 import { useRef, useState } from 'react';
 import usePreviewImg from '../hooks/usePreviewImg';
 import { BsFillImageFill } from 'react-icons/bs';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import userAtom from '../atoms/userAtom';
 import useShowToast from '../hooks/useShowToast';
+import postsAtom from '../atoms/postsAtom';
 const MAX_CHAR = 500;
 
 const CreatePost = () => {
@@ -35,6 +36,7 @@ const CreatePost = () => {
     const user = useRecoilValue(userAtom);
     const showToast = useShowToast();
     const [loading, setLoading] = useState(false);
+    const [posts, setPosts] = useRecoilState(postsAtom);
 
     const handleTextChange = (e) => {
         const inputText = e.target.value;
@@ -65,6 +67,7 @@ const CreatePost = () => {
                 return;
             }
             showToast('Success', 'Post created successfully', 'success');
+            setPosts([data, ...posts]);
             onClose();
             setPostText('');
             setImgUrl('');
